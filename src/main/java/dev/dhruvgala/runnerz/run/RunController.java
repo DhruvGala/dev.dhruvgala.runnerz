d2d828e05a89d788a2b655e919a75d4598cbebc2
Dhruv <dhruv.g123@gmail.com>
2024-07-13 21:04:31 -0700

    added basic CRUD

--- src/main/java/dev/dhruvgala/runnerz/run/RunController.java
+++ src/main/java/dev/dhruvgala/runnerz/run/RunController.java
@@ -1,10 +1,7 @@
 package dev.dhruvgala.runnerz.run;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -32,5 +29,25 @@ public class RunController {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
     return run.get();
+  }
+
+  @ResponseStatus(HttpStatus.CREATED)
+  @PostMapping("")
+  void createRun(@RequestBody Run run) {
+    runRepository.create(run);
+  }
+
+
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @PutMapping("/{id}")
+  void updateRun(@RequestBody Run run, @PathVariable Integer id) {
+    runRepository.update(run, id);
+  }
+
+
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  @DeleteMapping("/{id}")
+  void deleteRun(@PathVariable Integer id) {
+    runRepository.delete(id);
   }
 }
