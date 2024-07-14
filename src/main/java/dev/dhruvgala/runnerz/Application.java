cc3ebe4a2c8661f9aa2d6aab72f005551dd0b6ca
Dhruv <dhruv.g123@gmail.com>
2024-07-14 10:39:33 -0700

    adding in-mem db and pulling data from db

--- src/main/java/dev/dhruvgala/runnerz/Application.java
+++ src/main/java/dev/dhruvgala/runnerz/Application.java
@@ -1,22 +1,12 @@
 package dev.dhruvgala.runnerz;
 
-
-import dev.dhruvgala.runnerz.run.Location;
-import dev.dhruvgala.runnerz.run.Run;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
